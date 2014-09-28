fa0ad011676e00848db3af1aa3e7f1c6de264766
Robert Grant <rhgrant10@gmail.com>
2014-09-28 02:24:49 -0400

    Reworked problem 3

--- problem3.py
+++ problem3.py
@@ -5,35 +5,31 @@ Largest Prime Factors
 
 The prime factors of 13195 are 5, 7, 13 and 29.
 
-What is the largest prime factor of the number 600851475143 ?
+What is the largest prime factor of the number 600851475143?
+
 """
+
 from math import sqrt, ceil
 
 
-def is_prime(n):
-    if n < 3:
-        return n == 2
-    for i in range(2, ceil(sqrt(n)) + 1):
-        if n % i == 0:
-            return False
-    return True
+NUMBER = 600851475143
+
 
 def primes(limit):
-    if limit >= 2:
-        yield 2
-        n = 3
-        while n < limit:
-            if is_prime(n):
-                yield n
-            n += 2
+    is_prime = [False, False] + [True for _ in range(limit - 2)]
+    for i in range(2, limit):
+        if is_prime[i]:
+            for j in range(i + i, limit, i):
+                is_prime[j] = False
+    return [i for i, is_p in enumerate(is_prime) if is_p]
 
 def prime_factors(n):
-    for p in primes(sqrt(n) + 1):
+    for p in primes(int(sqrt(n) + 1)):
         if n % p == 0:
             yield p
 
 def answer():
-    return max(prime_factors(600851475143))
+    return max(prime_factors(NUMBER))
 
 
 if __name__ == '__main__':
