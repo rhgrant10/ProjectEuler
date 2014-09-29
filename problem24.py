88180223a351a10ba0048f711d568404ff75ed0c
Robert Grant <rhgrant10@gmail.com>
2014-09-29 15:25:09 -0400

    Inital attempt. "Killed"

---
+++ problem24.py
@@ -0,0 +1,26 @@
+"""Problem 24
+
+Lexicographic permutations
+=============
+
+A permutation is an ordered arrangement of objects. For example, 3124 is one
+possible permutation of the digits 1, 2, 3 and 4. If all of the permutations
+are listed numerically or alphabetically, we call it lexicographic order. The
+lexicographic permutations of 0, 1 and 2 are:
+
+    012   021   102   120   201   210
+
+What is the millionth lexicographic permutation of the digits
+0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+
+"""
+
+from itertools import permutations
+
+N = 10 ** 6
+
+def answer():
+    return sorted(permutations(str(range(10))))[N + 1]
+
+if __name__ == '__main__':
+    print(answer())
